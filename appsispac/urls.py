7ffe9b9689e53024e7335cdb9f161bfe9dcc8cec
Hallessandro <hd.jesus93@gmail.com>
2016-08-13 17:55:24 -0300

    Criando views iniciais

--- appsispac/urls.py
+++ appsispac/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import include, url
 from appsispac.views import *
 
 urlpatterns = [
-	 url(r'^$',home,name='home'),
-	 url(r'^frequencias/list$',frequencias_list,name='frequencias_list'),
-	 url(r'^frequencias/update/(?P<pk>\d+)$',frequencias_update,name='frequencias_update'),
+	url(r'^$', home, name='home'),
+	url(r'^funcionario/list$',funcionario_list,name='funcionario_list'),
 ]
--- appsispac/views.py
+++ appsispac/views.py
@@ -1,23 +1,28 @@
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from django.shortcuts import render,redirect
 from appsispac.models import *
 from appsispac.forms import *
 from django.forms import formset_factory
 # Create your views here.
 def home(request):
-    return render(request,'appsispac/base.html')
+    return render(request, 'base.html')
 
-def frequencias_list(request):
-    frequencias=Frequencia.objects.all()
-    return render(request, 'appsispac/frequencias_list.html', {'frequencias':frequencias})
-
-def frequencias_update(request, pk):
-    frequencias=Frequencia.objects.get(id=pk)
-    if (request.method=="POST"):
-        form=FrequenciaForm(request.POST,instance=frequencias)
-        if(form.is_valid()):
-            form.save()
-            return redirect('frequencias_list')
+def funcionario_list(request):
+    criterio = request.GET.get('criterio')
+    if(criterio):
+        funcionarios = Funcionario.objects.filter(nome__contains=criterio)
     else:
-        form=FrequenciaForm(instance=frequencias)
-        dados={'form':form,'frequencias':frequencias}
-    return render(request,'appsispac/frequencias_form.html',dados)
+        funcionarios = Funcionario.objects.all().order_by('nome')
+        criterio = ""
+    paginator = Paginator(funcionarios, 2)
+    page = request.GET.get('page')
+    try:
+        funcionarios = paginator.page(page)
+    except PageNotAnInteger:
+        funcionarios = paginator.page(1)
+    except EmptyPage:
+        funcionarios = paginator.page(paginator.num_pages)
+    dados = {'funcionarios': funcionarios, 'criterio': criterio, 'paginator': paginator, 'page_obj':funcionarios}
+    return render(request, 'funcionario/funcionario_list.html', dados)
+
+# def funcionario_update(request):
